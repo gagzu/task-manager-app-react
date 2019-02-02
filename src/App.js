d664b72f8ae3d6d26ca4744fa1f1c4f6b48306e7
gallbers <gagzu.ali@gmail.com>
2019-02-02 20:19:57 -0300

    Se agrego un componente formulario mara manejar el adicionar mas tareas y se agrego un botn para poder eliminar las tareas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// Data
+import  { tasks } from "./tasks.json"; 
+
+// component
+import TaskForm from "./components/TaskForm"; 
+
 
 class App extends Component {
+
+	constructor() {
+		super();
+		this.state = {
+			tasks
+		}
+		this.handleAddTask = this.handleAddTask.bind(this);
+	}
+
+	handleAddTask(task) {
+    this.setState({
+      tasks: [task, ...this.state.tasks]
+    })
+    console.log(this.state.tasks)
+  }
+
+  handleRemoveTask(index) {
+		this.setState({
+			tasks: this.state.tasks.filter(
+			(item, i) => {
+				return i !== index
+			})
+		})
+  }
+
   render() {
+		const tasks = this.state.tasks.map((task,i) => {
+			return (
+				<div className="col-md-4" key={i}>
+					<div className="card mt-4">
+						<div className="card-header">
+							<h3>{task.title}</h3>
+							<span className="badge badge-pill badge-danger ml-2">
+								{task.priority}
+							</span>
+						</div>
+						<div className="card-body">
+							<p>{task.description}</p>
+							<p><mark>{task.responsible}</mark></p>
+						</div>
+						<div className="card-footer">
+							<button
+								className="btn btn-danger"
+								onClick={this.handleRemoveTask.bind(this, i)}
+							>
+								Delete
+							</button>
+						</div>
+					</div>
+				</div>
+			)
+		})
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+				
+				<nav className="navbar navbar-dark bg-dark">
+					<a href="" className="text-white">
+						tasks
+						<span className="badge badge-pill badge-light ml-2">
+							{this.state.tasks.length}
+						</span>
+					</a>
+				</nav>
+
+    		<div className="container">
+    			<div className="row">
+		    		<div className="col-md-4 text-center mt-4">
+		    				<TaskForm onAddTask={this.handleAddTask}></TaskForm>
+		    				<img src={logo} className="App-logo" alt="logo" />
+		    		</div>
+
+	    			<div className="col-md-8">
+		    			<div className="row">
+		    				{tasks}
+		    			</div>
+	    			</div>
+    			</div>
+    		</div>
       </div>
     );
   }
